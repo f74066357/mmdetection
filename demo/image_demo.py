03d87ef32446187c8dec255ead32a2bf73efe8ab
f74066357 <f74066357@mail.ncku.edu.tw>
2020-09-27 11:46:12 +0800

    get coordinate

--- demo/image_demo.py
+++ demo/image_demo.py
@@ -3,6 +3,7 @@ from argparse import ArgumentParser
 from mmdet.apis import inference_detector, init_detector, show_result_pyplot
 
 
+
 def main():
     parser = ArgumentParser()
     parser.add_argument('img', help='Image file')
@@ -16,8 +17,33 @@ def main():
 
     # build the model from a config file and a checkpoint file
     model = init_detector(args.config, args.checkpoint, device=args.device)
+    
+    
+
+    
     # test a single image
     result = inference_detector(model, args.img)
+    
+    mythr=0.5
+    for i in range (18):
+      if(len(result[i])>0):
+        for j in range(len(result[i])):
+          if(result[i][j][4]>mythr):
+            print(result[i][j])
+        
+    #print(len(result[0]))
+    #print(len(result[17]))
+    #print((result[17]))
+    
+    #result[1][0][0]=100
+    #result[1][0][1]=500
+    #result[1][0][2]=800
+    #result[1][0][3]=200
+    #print(result[1][0][4])
+    #l u r d
+
+    #k=[[923.4591    332.5638    100.5444    403.03183     0.9977043]]
+    
     # show the results
     show_result_pyplot(model, args.img, result, score_thr=args.score_thr)
 
